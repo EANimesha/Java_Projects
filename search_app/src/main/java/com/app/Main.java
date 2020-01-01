3493538e1190a50e4034c5fb37de5cac6c395e7c
EANimesha <nimeshadilini999@gmail.com>
2020-01-01 15:44:36 +0530

    add user model and conveert json to object list with gson

--- search_app/src/main/java/com/app/Main.java
+++ search_app/src/main/java/com/app/Main.java
@@ -1,7 +1,21 @@
 package com.app;
 
+import com.app.models.User;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args)  throws IOException {
+
+        Gson gson=new Gson();
+        String userData=new String(Files.readAllBytes(Paths.get("D:\\Java\\Java_Projects\\search_app\\src\\main\\resources\\JsonStore\\users.json")));
+
+        User[] userArray= gson.fromJson(userData,User[].class);
+//        for(User user : userArray) {
+//            System.out.println(user.toString());
+//        }
     }
 }
--- search_app/src/main/java/com/app/models/User.java
+++ search_app/src/main/java/com/app/models/User.java
@@ -8,12 +8,104 @@ public class User {
     private String external_id;
     private String name;
     private String alias;
-    private Date created_at;
+    private String created_at;
     private boolean active;
     private boolean verified;
     private boolean shared;
     private String locale;
     private String timezone;
 
+    public String toString(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("id="+get_id()+"\n");
+        return sb.toString();
+    }
 
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getExternal_id() {
+        return external_id;
+    }
+
+    public void setExternal_id(String external_id) {
+        this.external_id = external_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+    public boolean isShared() {
+        return shared;
+    }
+
+    public void setShared(boolean shared) {
+        this.shared = shared;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
 }
