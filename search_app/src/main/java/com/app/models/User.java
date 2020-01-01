1e45a498d9a6d13e2f8dd168ccf09abd9a3b7479
EANimesha <nimeshadilini999@gmail.com>
2020-01-01 14:30:50 +0530

    new java maven project for search app

---
+++ search_app/src/main/java/com/app/models/User.java
@@ -0,0 +1,19 @@
+package com.app.models;
+
+import java.util.Date;
+
+public class User {
+    private int _id;
+    private  String url;
+    private String external_id;
+    private String name;
+    private String alias;
+    private Date created_at;
+    private boolean active;
+    private boolean verified;
+    private boolean shared;
+    private String locale;
+    private String timezone;
+
+
+}
